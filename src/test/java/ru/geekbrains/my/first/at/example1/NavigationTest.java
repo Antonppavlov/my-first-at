eec63b1fc47ed24f69b97e750dda38f24e4603c6
Antonppavlov <anton.it.pavlov@gmail.com>
2020-02-16 04:59:00 +0300

    настройка Jenkinsfile

--- src/test/java/ru/geekbrains/my/first/at/example1/NavigationTest.java
+++ src/test/java/ru/geekbrains/my/first/at/example1/NavigationTest.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.my.first.at.example1;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
@@ -8,22 +9,22 @@ public class NavigationTest {
     @Test
     public void checkNavigation(){
         Selenide.open("https://geekbrains.ru/courses");
-//        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/courses\"]").click();
-//        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Курсы"));
-//
-//        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/events\"]").click();
-//        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Вебинары"));
-//
-//        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/topics\"]").click();
-//        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Форум"));
-//
-//        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/posts\"]").click();
-//        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Блог"));
-//
-//        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/tests\"]").click();
-//        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Тесты"));
-//
-//        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/career\"]").click();
-//        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Карьера"));
+        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/courses\"]").click();
+        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Курсы"));
+
+        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/events\"]").click();
+        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Вебинары"));
+
+        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/topics\"]").click();
+        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Форум"));
+
+        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/posts\"]").click();
+        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Блог"));
+
+        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/tests\"]").click();
+        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Тесты"));
+
+        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/career\"]").click();
+        Selenide.$("[class=\"gb-header__title\"]").shouldHave(Condition.exactText("Карьера"));
     }
 }
